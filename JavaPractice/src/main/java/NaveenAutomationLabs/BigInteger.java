6aaafd0b3cd57a8d308cf6992b35d5b75d8cf561
naveen <nkcs2u@gmail.com>
2022-12-16 17:12:35 +0530

    6th edit

---
+++ JavaPractice/src/main/java/NaveenAutomationLabs/BigInteger.java
@@ -0,0 +1,13 @@
+package NaveenAutomationLabs;
+
+public class BigInteger {
+    /*
+    long 64bit
+
+    BigInteger n11 = new BigInteger("12122221221221112112222212121212");
+    BigInteger n2 = same as above 64 bit
+
+    BigInteger sum = n1.add(n2);
+    sysout sum
+     */
+}
---
+++ JavaPractice/src/main/java/NaveenAutomationLabs/memory.java
@@ -0,0 +1,12 @@
+package NaveenAutomationLabs;
+
+public class memory {
+    /*
+    heap : object creation memory
+
+    stack : execution flow from main() method.
+    next statments, local variables in method.
+
+    if infinite loop/recursion, chances of stack overflow error.
+     */
+}
